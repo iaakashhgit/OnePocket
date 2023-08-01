ea3cb8501221204161378daf84bec7195b53a620
iaakashhgit <57087596+iaakashhgit@users.noreply.github.com>
2023-08-01 11:47:25 +0530

    latest changes

--- Task3/fileRead.js
+++ Task3/fileRead.js
@@ -1,7 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
 
 const countWordsInFile = async () => {
-    console.log("hi")
+  const filename = 'data.txt';
+  try {
+    const data = await fs.promises.readFile(filename, 'utf8');
+    const wordCount = countWords(data);
+    console.log(`Total word count: ${wordCount}`);
+  } catch (err) {
+    console.error(`Error reading file: ${err}`);
+  }
 }
 
-module.exports = {countWordsInFile}
+const countWords = (text) => {
+  const trimmedText = text.trim();
+  const wordsArray = trimmedText.split(/\s+/);
+  return wordsArray.length
+}
+
+// Call the async function to count words in the file
+countWordsInFile();
